d458ded2aafaca7c810998dee2050f8efeb47a65
ShuyangLiu <sliu44@u.rochester.edu>
2016-03-09 14:31:54 -0500

    initial commit

---
+++ src/KB.java
@@ -0,0 +1,9 @@
+/*
+* A Knowledge Base that consists a list of sentences
+*   A sentence is a boolean value that constructed using variables and syntax
+* */
+public class KB
+{
+
+
+}
---
+++ src/Syntax.java
@@ -0,0 +1,33 @@
+/*A class of logical syntax*/
+
+public class Syntax
+{
+    public static boolean NOT(boolean var)
+    {
+        return !var;
+    }
+
+    public static boolean AND(boolean a , boolean b)
+    {
+        return a && b;
+    }
+
+    public static boolean OR(boolean a , boolean b)
+    {
+        return a || b;
+    }
+
+    public static boolean XOR(boolean a , boolean b)
+    {
+        return a ^ b;
+    }
+
+    public static boolean IMPLY(boolean a , boolean b) {
+        return !a || b;
+    }
+
+    public static boolean IFF(boolean a , boolean b)
+    {
+        return(Syntax.IMPLY(a, b) && Syntax.IMPLY(b, a));
+    }
+}
